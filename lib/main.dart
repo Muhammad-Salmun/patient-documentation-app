913920d38f52e15f67dac9ed066cb9c2ad435b6b
Muhammad-Salmun <muhammedsalmun@gmail.com>
2025-04-15 16:25:40 +0530

    updated version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:surgery_doc/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -9,6 +8,5 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
